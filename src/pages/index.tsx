a14cadefe278cb5729686a82fb101a041b6c9bbc
Martin Ly <supernoob727@gmail.com>
2023-04-13 11:08:29 +1000

    Initial commit from Create Next App

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { LandingPage } from "./LandingPage";
+import { HomePage } from "./HomePage";
+
+export const Index: React.FC = () => {
+  return (
+    <>
+      <LandingPage />
+    </>
+  )
+}
+
